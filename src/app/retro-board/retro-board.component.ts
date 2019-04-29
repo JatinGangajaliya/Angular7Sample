286b5a9a6f6a4b39d9c5e9aa90afe5ec27dbd3a7
Krunal Modi <krunal.modi@amobee.com>
2019-04-29 18:16:48 +0530

    Creation of Routing of Retro board and Angular material design integration
    Signed-off-by: Krunal Modi <krunal.modi@amobee.com>

---
+++ src/app/retro-board/retro-board.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-retro-board',
+  templateUrl: './retro-board.component.html',
+  styleUrls: ['./retro-board.component.scss']
+})
+export class RetroBoardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
